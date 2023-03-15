ecb194f57249373daeaa233072e3d575e2fa7a77
Goongam <dlwjddn341@naver.com>
2023-03-15 14:49:22 +0900

    DAY 32

--- 520/10971.js
+++ 520/10971.js
@@ -5,22 +5,67 @@ try {
 } catch (error) {
 input =
 `4
-1 2 3 4`.trim().split('\n');
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0`.trim().split('\n');
 }
 
 
 let length = +input.shift();
 let nums = new Array(+length).fill(0).map((_,index) => index+1);
 
-let find = input.shift().split(' ').map(n => +n);
+let citys = input.map(line => line.split(' ').map(n => +n));
+
+// console.log(citys);
+//세로, 가로, 0 ~ length-1까지
 
 let visit = new Array(+length).fill(false);
-visit[0] = undefined;
+
 let result = [];
 let arr = [];
 
-find.forEach(n => visit[n] = true);
-
 // 중복을 허용하지 않는 도시의 개수만큼 count가 있는 재귀 후 마지막 돌아오는 비용만 더해줌.
 // ex) A->B, A->C, A->D  /   B->C, B->D   /   C->B, C->D ...
+
+let min = Infinity;
+
+function dfs(count){
+    if(count >= length){
+        
+        
+       let sum = 0;
+        for(let i = 1; i < length; i++){
+            let cost = citys[arr[i]][arr[i-1]];
+            if(cost === 0) return;
+            sum += cost;
+        }
+        
+        let cost = citys[arr[0]][arr[length-1]];
+        if(cost === 0) return;
+        sum += cost;
+        
+        if(min > sum) min = sum;
+        
+        return;
+    }
+// console.log(count);
+
+    for(let i = 0; i < length; i++){
+        if(visit[i]) continue;
+        visit[i] = true;
+        
+        arr.push(i);
+        dfs(count+1);
+        arr.pop();
+
+        visit[i] = false;
+    }
+
+}
+
+dfs(0);
+
+console.log(min);
+
 
