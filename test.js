3f3bca5e887e7e4ef1a6a2c5abaa3bbf956be8a9
Goongam <dlwjddn341@naver.com>
2023-04-23 21:51:32 +0900

    1149

--- test.js
+++ test.js
@@ -1,27 +1,42 @@
 const fs = require("fs");
 let input;
 try {
-  input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+  input = fs.readFileSync("/dev/stdin").toString().trim();
 } catch {
-input = `2
-3 ABC
-5 /HTP`
+input = `QWERQWER`
     .toString()
-    .trim()
-    .split("\n");
+    .trim();
 }
 
-input.shift();
+if(input.length === 0){
+  process.exit();
+}
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+let count = new Array(26).fill(0);
 
-input.forEach(line =>{
-  const [length, str] = line.split(' ');
-  let result = "";
-  str.split('').forEach((s)=>{
-      for(let i = 0; i < length ; i++){
-          result += s;
-      }
-  });
-  console.log(result);
-  
+input = input.toLowerCase();
+
+input.split('').forEach((c)=>{
+  let idx = alpha.indexOf(c);
+  count[idx]++;
 });
 
+let result;
+let max = Math.max(...count);
+let maxIdx = count.indexOf(max);
+
+let maxcount = 0;
+count.forEach((num) => {
+  if(num === max) maxcount++;
+});
+
+if(maxcount >= 2) result = '?';
+else{
+  result = alpha[maxIdx].toUpperCase();
+}
+
+
+console.log(result);
+
+
+// console.log(alpha[]);
